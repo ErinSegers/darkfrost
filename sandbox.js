0583875778f158cf972e7a5ff9be6cf0f45abfd6
Erin McCaffrey <erinmccaffrey@Erins-MacBook-Pro.local>
2017-02-27 19:54:44 -0500

    committing changes

---
+++ sandbox.js
@@ -0,0 +1,13 @@
+// var axios = require('axios');
+// var apiKey = require('./secrets').darkskyAPIKey;
+// var url = `https://api.darksky.net/forecast/${apiKey}/37.8267,-122.4233`;
+//
+// axios.get(url)
+//       .then(function(response){
+//         console.log(response.data);
+//       })
+//       .catch(function(err){
+//         console.log(err);
+//       });
+
+//test environment, for learning
