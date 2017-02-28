1c7b17c21bf08389b8e6f6776783252de70de3d7
Erin McCaffrey <erinmccaffrey@Erins-MacBook-Pro.local>
2017-02-27 20:54:04 -0500

    successfully implemented a Vue application that displays the crrent weather data from the Vue instance

---
+++ public/js/app.js
@@ -0,0 +1,12 @@
+var currentlyWidget = new Vue({
+  el: '#currently',
+  data: {
+    location: 'Gainesville, FL',
+    time: 1000000,
+    summary: 'Partly cloudy',
+    icon: 'partly-cloudy',
+    apparentTemperature: 75.4,
+    precipProbability: 0.30,
+    humidity: 0.45
+  }
+});
