c1f18b82847fd882ae3f2932c98ccda138939280
Erin McCaffrey <erinmccaffrey@Erins-MacBook-Pro.local>
2017-03-01 20:18:19 -0500

    created a form that dynamically pulls in weather for input latitude and longitude upon click update weather

--- public/js/app.js
+++ public/js/app.js
@@ -7,11 +7,29 @@ var currentlyWidget = new Vue({
     icon: 'clear-day',
     apparentTemperature: 75.4,
     precipProbability: 0.30,
-    humidity: 0.45
+    humidity: 0.45,
+    latitude: 29.1,
+    longitude: -81.4
   },
   methods: {
     iconUrl: function(iconString){
       return `/images/${iconString}.png`;
+    },
+    updateWeather: function(){
+      var url = `/weather/${this.latitude},${this.longitude}`;
+      axios.get(url)
+          .then(function(response){
+            var data = response.data.currently;
+            currentlyWidget.time = data.time;
+            currentlyWidget.summary = data.summary;
+            currentlyWidget.icon = data.icon;
+            currentlyWidget.apparentTemperature = data.apparentTemperature;
+            currentlyWidget.precipProbability = data.precipProbability;
+            currentlyWidget.humidity = data.humidity;
+          })
+          .catch(function(err){
+            console.log(err);
+          });
     }
   },
   created: function(){
